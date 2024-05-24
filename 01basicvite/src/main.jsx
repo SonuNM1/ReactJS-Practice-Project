ea668082cb65511b94d151d2a0bc1e7ba01c14bf
Sonu N Mahto <sonu.mahto362000@gmail.com>
2024-05-24 19:11:46 +0530

    Counter game

--- 01basicvite/src/main.jsx
+++ 01basicvite/src/main.jsx
@@ -1,10 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
 
+const areactElement = React.createElement(
+  "a",
+  { href: "https://google.com", target: "_blank" },
+  "Click to visit Google"
+);
 
-ReactDOM.createRoot(document.getElementById('root')).render(
- 
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <>
+    <areactElement />
     <App />
- 
-)
+  </>
+);
