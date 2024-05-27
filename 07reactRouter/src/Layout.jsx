3b8a2fbdbbc31d4680de4826a34c08f12e512add
Sonu N Mahto <sonu.mahto362000@gmail.com>
2024-05-27 21:02:36 +0530

    27th may

---
+++ 07reactRouter/src/Layout.jsx
@@ -0,0 +1,17 @@
+
+import React from 'react'
+import {Outlet} from 'react-router-dom'
+import Header from './components/header/header'
+import Footer from './components/footer/footer'
+
+function Layout() {
+  return (
+    <>
+        <Header/>
+        <Outlet/>
+        <Footer/>
+    </>
+  )
+}
+
+export default Layout
